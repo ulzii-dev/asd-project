797954a1d02276c46c5f1b9221fb770053ba0bf0
Bijay Shrestha <bijayshrestha@Bijays-MacBook-Pro.local>
2022-03-15 12:09:59 -0500

    Removing CheckingAccounts. Minor fix in CheckingAccount and SavingAccount.

--- Code/backend/commons/CheckingAccounts.java
+++ Code/backend/commons/CheckingAccounts.java
@@ -3,17 +3,17 @@ package backend.commons;
 import backend.banking.visitor.Visitable;
 import backend.banking.visitor.Visitor;
 
-public class CheckingAccounts extends Account {
-    public CheckingAccounts(String accountNumber) {
-        super(accountNumber);
-    }
-
-    @Override
-    public double accept(Visitor visitor) {
-        return visitor.visit(this);
-    }
-
-    public String getAccountTypes(){
-        return AccountType.Checking.name();
-    }
-}
+//public class CheckingAccounts extends Account implements Visitable {
+//    public CheckingAccounts(String accountNumber) {
+//        super(accountNumber);
+//    }
+//
+//    @Override
+//    public double accept(Visitor visitor) {
+//        return visitor.visit(this);
+//    }
+//
+//    public String getAccountTypes(){
+//        return AccountType.Checking.name();
+//    }
+//}
--- Code/backend/commons/SavingsAccount.java
+++ Code/backend/commons/SavingsAccount.java
@@ -7,12 +7,10 @@ public class SavingsAccount extends Account implements Visitable {
     public SavingsAccount(InterestComputationStrategy interestComputationStrategy) {
         super(interestComputationStrategy);
     }
-
     @Override
     public String getAccountType() {
         return AccountType.Saving.name();
     }
-
     public double accept(Visitor visitor) {
         return visitor.visit(this);
     }

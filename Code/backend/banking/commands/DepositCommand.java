d8ce2a53dbe258c066987871b748419d43ac2533
Saruul <s.saruululzii@gmail.com>
2022-03-15 12:55:06 -0500

    Deposit and Withdraw commands

--- Code/backend/banking/commands/DepositCommand.java
+++ Code/backend/banking/commands/DepositCommand.java
@@ -3,8 +3,16 @@ package backend.banking.commands;
 import backend.commons.BankAccountService;
 
 public class DepositCommand implements Command {
+    private String accountNumber;
+    private double amount;
+
+    public DepositCommand(String accNumber, double amount) {
+        this.accountNumber = accNumber;
+        this.amount = amount;
+    }
+
     @Override
     public void execute() {
-        BankAccountService.getInstance().deposit("", 0d);
+        BankAccountService.getInstance().deposit(this.accountNumber, this.amount);
     }
 }
--- Code/backend/banking/commands/WithdrawCommand.java
+++ Code/backend/banking/commands/WithdrawCommand.java
@@ -3,8 +3,16 @@ package backend.banking.commands;
 import backend.commons.BankAccountService;
 
 public class WithdrawCommand implements Command {
+    private String accountNumber;
+    private double amount;
+
+    public WithdrawCommand(String accNumber, double amount) {
+        this.accountNumber = accNumber;
+        this.amount = amount;
+    }
+
     @Override
     public void execute() {
-        BankAccountService.getInstance().withdraw("", 0d);
+        BankAccountService.getInstance().withdraw(this.accountNumber, this.amount);
     }
 }

c2197f146957f054917ff8f8bba95700cec88154
Saruul <s.saruululzii@gmail.com>
2022-03-15 10:58:20 -0500

    AddInterest.java renamed to AddInterestCommand.java

--- Code/backend/banking/commands/AddInterestCommand.java
+++ Code/backend/banking/commands/AddInterestCommand.java
@@ -2,7 +2,7 @@ package edu.mum.cs.cs525.labs.exercises.project.backend.banking.commands;
 
 import edu.mum.cs.cs525.labs.exercises.project.backend.commons.BankAccountService;
 
-public class AddInterest implements Command{
+public class AddInterestCommand implements Command{
     @Override
     public void execute() {
         BankAccountService.getInstance().addInterest();

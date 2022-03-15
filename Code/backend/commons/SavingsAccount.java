6132cc2eaad77cab825ecb39298448840595d76b
Bijay Shrestha <bijayshrestha@Bijays-MacBook-Pro.local>
2022-03-15 11:54:29 -0500

    Minor fix in Savings account

--- Code/backend/commons/SavingsAccount.java
+++ Code/backend/commons/SavingsAccount.java
@@ -17,7 +17,5 @@ public class SavingsAccount extends Account implements Visitable {
         return visitor.visit(this);
     }
 
-    public String getAccountTypes(){
-        return AccountTypes.Saving.name();
-    }
+
 }

28bbd1edfabd6ac909185b4df9d334f1bcc2102c
yohannes84 <34288731+yohannes84@users.noreply.github.com>
2022-03-16 17:15:14 -0500

    name change

---
+++ Code/backend/creditcard/strategy/GoldCreditCardCalculator.java
@@ -0,0 +1,26 @@
+package backend.creditcard.strategy;
+
+import backend.commons.InterestComputationStrategy;
+
+public class GoldCreditCardcalculator implements CreditCardCalculator{
+
+    @Override
+    public double computeInterest(double totalCredit) {
+        return totalCredit * 0.06;
+    }
+
+    @Override
+    public double computeMonthlyMinimumPayment(double totalCredit) {
+        return totalCredit * 0.1;
+    }
+
+    @Override
+    public double computeBalance(double previousBalance, double totalCredit, double totalCharges) {
+        return previousBalance - totalCredit + totalCharges + 0.1 * (previousBalance - totalCredit);
+    }
+
+    @Override
+    public double computeTotalDue(double newBalance) {
+        return newBalance * 0.1;
+    }
+}
